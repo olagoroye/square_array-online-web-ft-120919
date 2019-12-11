8efc3258d8f072e7f6705a438abbeea31f034301
olusola olagoroye <solaolagoroye@gmail.com>
2019-12-11 01:54:40 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -9,13 +9,3 @@ return new_array
 end
 
 
-# def square_array(array)
-# counter = 0
-# new_array = []
-#   while array[counter] do
-#     new_array << (array[counter] ** 2)
-#     counter += 1
-    
-#   end
-#   return new_array
-# end
