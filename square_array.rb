bc68dca04937f597376a1017c957f3854473f813
olusola olagoroye <solaolagoroye@gmail.com>
2019-12-11 01:47:04 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,21 @@
 def square_array(array)
-  # your code here
+counter = 0 
+new_array = []
+array.each do |array|
+  new_array << (array[counter] ** 2)
+  counter += 1
 end
+return new_array
+end
+
+
+# def square_array(array)
+# counter = 0
+# new_array = []
+#   while array[counter] do
+#     new_array << (array[counter] ** 2)
+#     counter += 1
+    
+#   end
+#   return new_array
+# end
